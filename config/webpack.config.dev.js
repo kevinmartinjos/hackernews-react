284306bf1589dcabf2268fed13e37ef97c447081
Kevin <kevin@squadrun.co>
2018-06-27 18:59:21 +0530

    Fixing config issues

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -124,12 +124,6 @@ module.exports = {
                 include: paths.appSrc,
             },
             {
-                test: /\.js$/,
-                loader: 'flowtype-loader',
-                enforce: 'pre',
-                exclude: /node_modules/
-            },
-            {
                 // "oneOf" will traverse all following loaders until one will
                 // match the requirements. When no loader matches it will fall
                 // back to the "file" loader at the end of the loader list.
@@ -285,8 +279,7 @@ module.exports = {
         // solution that requires the user to opt into importing specific locales.
         // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
         // You can remove this if you don't use Moment.js:
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new FlowtypePlugin()
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
     ],
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
